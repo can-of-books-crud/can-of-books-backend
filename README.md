69284850a0070090029dea8e398d2f598916291c
Jchips <20979096+Jchips@users.noreply.github.com>
2023-05-24 19:12:55 -0700

    setup the post request

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ Books are life-changing. They have the power to enlighten, educate, entertain, h
 ## Use this template
 
 Clone this repo to your own account with the green "Use this template" button. Then, add any collaborators. Now you are ready to start adding features! Deploy your site to let the world know which books have had the greatest impact on you, and what's recommended. Enjoy!
+
+## Time estimates
+
+Name of feature: CREATE
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 6:47pm
+
+Finish time: _____
+
+Actual time needed to complete: _____
--- server.js
+++ server.js
@@ -4,10 +4,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const getBooks = require('./Modules/getBooks');
+const bookHandler = require('./Modules/bookHandler');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
@@ -20,12 +21,14 @@ db.once('open', () => console.log('Mongoose is connected'));
 
 app.get('/', (req, res) => res.status(200).send('Default route is working'));
 
-app.get('/books', getBooks);
+app.get('/books', bookHandler.getBooks);
+
+app.post('/books', bookHandler.addBook);
 
 app.get('/test', (request, response) => {
 
-  response.send('test request received')
+  response.send('test request received');
 
-})
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
