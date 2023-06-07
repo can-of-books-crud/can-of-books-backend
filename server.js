8e6fce6452523b62e46339d2fb22e3332c1df7fb
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-06-06 23:26:33 -0500

    implemented auth0

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const bookHandler = require('./Modules/bookHandler');
 const notFound = require('./Modules/notFound');
+const verifyUser = require('./Modules/Authorize');
 
 const app = express();
 app.use(cors());
@@ -21,8 +22,10 @@ db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', () => console.log('Mongoose is connected'));
 
 app.get('/', (req, res) => res.status(200).send('Default route is working'));
+app.use(verifyUser);
 
 app.get('/books', bookHandler.getBooks);
+
 
 app.post('/books', bookHandler.addBook);
 
